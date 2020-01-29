0f36c6f1c84d363257a7fd53ac0dd8c5f0eb83cf
Dan Bishop <56052905+bishdj22@users.noreply.github.com>
2020-01-29 12:53:43 -0500

    All final tables loaded to pgAdmin, SQL joins in progress

--- pgAdmin/mergedzillowtablequery.sql
+++ pgAdmin/mergedzillowtablequery.sql
@@ -11,10 +11,38 @@ SELECT zillow_db.zipcode, zillow_db.city, zillow_db.state,
     LEFT JOIN zillow_db ON (zillow_db.zipcode = zillow_2018_db.zipcode);
 	
 Select * FROM merged_zillow;
-	
+
+--Mahesh Query
 select * from single_family_property_type where year =2016;
 select * from single_family_property_type where year =2018;
 select s.year,s.bank,s.tractrat,s.income,s.bocreditscore,s.ltv,s.amount,s.rate,z.zip,z.city 
 from single_family_property_type s,zip_table z 
 where s.stcountyfp=z.county_fips	
+
+Select * FROM merged_zillow;
+
+Drop Table zillow_db;
+
+--DB checking query to ensure only 2016, 2018 included--
+SELECT * FROM fhlb_housing_zip 
+WHERE year = 2016 OR year = 2018;
+
+
+SELECT COUNT(*) FROM fhlb_housing_zip;
+SELECT COUNT(*) FROM merged_zillow;
+SELECT * FROM fhlb_housing_zip;
+SELECT * FROM merged_zillow;
+
+
+SELECT fhlb_housing_zip.year, fhlb_housing_zip.bank,
+	   fhlb_housing_zip.tractrat, fhlb_housing_zip.income, fhlb_housing_zip.bocreditscore, 
+	   fhlb_housing_zip.ltv, fhlb_housing_zip.amount, fhlb_housing_zip.rate, fhlb_housing_zip.zip,merged_zillow.zipcode, merged_zillow.city, merged_zillow.state,
+       merged_zillow.metro, merged_zillow.countyname, merged_zillow.avghomevalue2016, 
+	   merged_zillow.avghomevalue2018
+	---INTO single_fam_data_master
+	FROM merged_zillow
+    LEFT JOIN fhlb_housing_zip ON (merged_zillow.zipcode = fhlb_housing_zip.zip);
+	
+
+
 
