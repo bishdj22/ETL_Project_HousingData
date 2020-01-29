b3a270243a0db46be07b8abf5b4c0193b1cea0f9
Dan Bishop <56052905+bishdj22@users.noreply.github.com>
2020-01-29 14:06:31 -0500

    DB added Master single fam dataset joins complete

--- pgAdmin/mergedzillowtablequery.sql
+++ pgAdmin/mergedzillowtablequery.sql
@@ -23,26 +23,32 @@ Select * FROM merged_zillow;
 
 Drop Table zillow_db;
 
---DB checking query to ensure only 2016, 2018 included--
-SELECT * FROM fhlb_housing_zip 
-WHERE year = 2016 OR year = 2018;
-
-
 SELECT COUNT(*) FROM fhlb_housing_zip;
 SELECT COUNT(*) FROM merged_zillow;
+
 SELECT * FROM fhlb_housing_zip;
 SELECT * FROM merged_zillow;
 
+---USE INNER JOIN TO MERGE TABLES ON COMMON ZIP CODE VALUES---
 
-SELECT fhlb_housing_zip.year, fhlb_housing_zip.bank,
+DROP TABLE single_fam_data_master;
+
+SELECT fhlb_housing_zip.zip,fhlb_housing_zip.year, fhlb_housing_zip.bank,
 	   fhlb_housing_zip.tractrat, fhlb_housing_zip.income, fhlb_housing_zip.bocreditscore, 
-	   fhlb_housing_zip.ltv, fhlb_housing_zip.amount, fhlb_housing_zip.rate, fhlb_housing_zip.zip,merged_zillow.zipcode, merged_zillow.city, merged_zillow.state,
+	   fhlb_housing_zip.ltv, fhlb_housing_zip.amount, fhlb_housing_zip.rate, merged_zillow.zipcode, merged_zillow.city, merged_zillow.state,
        merged_zillow.metro, merged_zillow.countyname, merged_zillow.avghomevalue2016, 
 	   merged_zillow.avghomevalue2018
-	---INTO single_fam_data_master
+	INTO single_fam_data_master
 	FROM merged_zillow
-    LEFT JOIN fhlb_housing_zip ON (merged_zillow.zipcode = fhlb_housing_zip.zip);
-	
+    INNER JOIN fhlb_housing_zip ON (merged_zillow.zipcode = fhlb_housing_zip.zip);
+
+---TEST QUERY WHOLE TABLE---
+
+SELECT * FROM single_fam_data_master
+
+---CHECKING FOR NULL VALUES---
+
+SELECT * FROM single_fam_data_master WHERE NOT (single_fam_data_master IS NOT NULL);
 
 
 
